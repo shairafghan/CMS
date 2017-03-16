55f7d9cd711a8379e184fbcaf6b9652e55be6ad1
shairafghan <shair4afg@yahoo.com>
2017-03-16 16:17:11 +0500

    some changes to doctor view

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,5 @@
 class Patient < ApplicationRecord
-  validates :pat_name, presence: true, length: {maximum: 4}
+  validates :pat_name, presence: true, length: {maximum: 50}
   GENDER_TYPES = ["Male", "Female"]
   validates :pat_address, presence: true
 
